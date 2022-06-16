03e283c85800496b60fb757d68a7df2821fb7a90
Michael Rodler <contact@f0rki.at>
2022-06-16 11:04:17 +0200

    fix build for ARM target (#4)

--- target/arm/translate.c
+++ target/arm/translate.c
@@ -5558,12 +5558,12 @@ static bool op_s_rri_rot(DisasContext *s, arg_s_rri_rot *a,
       TCGv tmp1_64 = tcg_temp_new();
       TCGv tmp2_64 = tcg_temp_new();
       tcg_gen_extu_i32_i64(tmp1_64, tmp1);
-      tcg_gen_extu_i32_i64(tmp2_64, tmp2);
+      tcg_gen_extu_i32_i64(tmp2_64, tcg_constant_i32(imm));
       libafl_gen_cmp(s->pc_curr, tmp1_64, tmp2_64, MO_32);
       tcg_temp_free(tmp1_64);
       tcg_temp_free(tmp2_64);
 #else
-      libafl_gen_cmp(s->pc_curr, tmp1, tmp2, MO_32);
+      libafl_gen_cmp(s->pc_curr, tmp1, tcg_constant_i32(imm), MO_32);
 #endif
     }
 

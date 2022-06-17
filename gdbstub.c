dd8c52ec4893dcf199a76d4517e267fa9e99841a
Andrea Fioraldi <andreafioraldi@gmail.com>
2022-06-17 14:23:34 +0200

    test

--- gdbstub.c
+++ gdbstub.c
@@ -951,6 +951,7 @@ static const char *get_feature_xml(const char *p, const char **newp,
 
 static int gdb_read_register(CPUState *cpu, GByteArray *buf, int reg)
 {
+    fprintf(stderr, "CPUState %p\n", cpu);
     CPUClass *cc = CPU_GET_CLASS(cpu);
     CPUArchState *env = cpu->env_ptr;
     GDBRegisterState *r;

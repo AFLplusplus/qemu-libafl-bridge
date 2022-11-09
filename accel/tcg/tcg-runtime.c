272927633c1d3bff09b8c98906ba9b23c1ab4a17
Alwin Berger <alwin.berger@tu-dortmund.de>
2022-11-09 13:17:53 +0100

    systemmode: allow synchronous snapshotting
    Add a flag to take snapshots synchronosly.
    This should be used to take or load snapshots while the emulator is not
    running.

--- accel/tcg/tcg-runtime.c
+++ accel/tcg/tcg-runtime.c
@@ -43,8 +43,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void libafl_save_qemu_snapshot(char *name);
-void libafl_load_qemu_snapshot(char *name);
+void libafl_save_qemu_snapshot(char *name, bool sync);
+void libafl_load_qemu_snapshot(char *name, bool sync);
 
 static void save_snapshot_cb(void* opaque)
 {
@@ -57,8 +57,20 @@ static void save_snapshot_cb(void* opaque)
     free(opaque);
 }
 
-void libafl_save_qemu_snapshot(char *name)
+void libafl_save_qemu_snapshot(char *name, bool sync)
 {
+    // use snapshots synchronously, use if main loop is not running
+    if (sync) {
+        //TODO: eliminate this code duplication
+        //by passing a heap-allocated buffer from rust to c,
+        //which c needs to free
+        Error *err = NULL;
+        if(!save_snapshot(name, true, NULL, false, NULL, &err)) {
+            error_report_err(err);
+            error_report("Could not save snapshot");
+        }
+        return;
+    }
     char* name_buffer = malloc(strlen(name)+1);
     strcpy(name_buffer, name);
     aio_bh_schedule_oneshot_full(qemu_get_aio_context(), save_snapshot_cb, (void*)name_buffer, "save_snapshot");
@@ -84,8 +96,27 @@ static void load_snapshot_cb(void* opaque)
     free(opaque);
 }
 
-void libafl_load_qemu_snapshot(char *name)
+void libafl_load_qemu_snapshot(char *name, bool sync)
 {
+    // use snapshots synchronously, use if main loop is not running
+    if (sync) {
+        //TODO: see libafl_save_qemu_snapshot
+        Error *err = NULL;
+
+        int saved_vm_running = runstate_is_running();
+        vm_stop(RUN_STATE_RESTORE_VM);
+
+        bool loaded = load_snapshot(name, NULL, false, NULL, &err);
+
+        if(!loaded) {
+            error_report_err(err);
+            error_report("Could not load snapshot");
+        }
+        if (loaded && saved_vm_running) {
+            vm_start();
+        }
+        return;
+    }
     char* name_buffer = malloc(strlen(name)+1);
     strcpy(name_buffer, name);
     aio_bh_schedule_oneshot_full(qemu_get_aio_context(), load_snapshot_cb, (void*)name_buffer, "load_snapshot");

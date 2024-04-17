2edf778b1c21419dad0bf3b0ef487e7eaa6751f7
Romain Malmain <romain.malmain@pm.me>
2024-04-17 18:08:38 +0200

    Add utils (#62)
    * add utils file
    * user only guard.

---
+++ libafl/utils.c
@@ -0,0 +1,24 @@
+#include "qemu/osdep.h"
+
+#ifndef CONFIG_USER_ONLY
+#include "exec/memory.h"
+#include "qemu/rcu.h"
+#include "cpu.h"
+
+#include "libafl/utils.h"
+
+uint8_t* libafl_paddr2host(CPUState* cpu, hwaddr addr, bool is_write)
+{
+    if (addr == -1) {
+        return NULL;
+    }
+
+    hwaddr xlat;
+    MemoryRegion* mr;
+    WITH_RCU_READ_LOCK_GUARD() {
+        mr = address_space_translate(cpu->as, addr, &xlat, NULL, is_write, MEMTXATTRS_UNSPECIFIED);
+    }
+
+    return qemu_map_ram_ptr(mr->ram_block, xlat);
+}
+#endif

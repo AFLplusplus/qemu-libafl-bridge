5da5ec7482ff32a19355b67555b4e55132092039
Romain Malmain <romain.malmain@pm.me>
2024-04-19 11:34:09 +0200

    LibAFL guard

--- tcg/tcg-op-ldst.c
+++ tcg/tcg-op-ldst.c
@@ -1257,9 +1257,13 @@ void tcg_gen_atomic_##NAME##_i32_chk(TCGv_i32 ret, TCGTemp *addr,       \
     tcg_debug_assert(addr_type == tcg_ctx->addr_type);                  \
     tcg_debug_assert((memop & MO_SIZE) <= MO_32);                       \
     if (tcg_ctx->gen_tb->cflags & CF_PARALLEL) {                        \
+/* --- Start LibAFL code --- */                                         \
         libafl_gen_read(addr, make_memop_idx(memop, 0));                \
+/* --- End LibAFL code --- */                                           \
         do_atomic_op_i32(ret, addr, val, idx, memop, table_##NAME);     \
+/* --- Start LibAFL code --- */                                         \
         libafl_gen_write(addr, make_memop_idx(memop, 0));               \
+/* --- End LibAFL code --- */                                           \
     } else {                                                            \
         do_nonatomic_op_i32(ret, addr, val, idx, memop, NEW,            \
                             tcg_gen_##OP##_i32);                        \
@@ -1272,9 +1276,13 @@ void tcg_gen_atomic_##NAME##_i64_chk(TCGv_i64 ret, TCGTemp *addr,       \
     tcg_debug_assert(addr_type == tcg_ctx->addr_type);                  \
     tcg_debug_assert((memop & MO_SIZE) <= MO_64);                       \
     if (tcg_ctx->gen_tb->cflags & CF_PARALLEL) {                        \
+/* --- Start LibAFL code --- */                                         \
         libafl_gen_read(addr, make_memop_idx(memop, 0));                \
+/* --- End LibAFL code --- */                                           \
         do_atomic_op_i64(ret, addr, val, idx, memop, table_##NAME);     \
+/* --- Start LibAFL code --- */                                         \
         libafl_gen_write(addr, make_memop_idx(memop, 0));               \
+/* --- End LibAFL code --- */                                           \
     } else {                                                            \
         do_nonatomic_op_i64(ret, addr, val, idx, memop, NEW,            \
                             tcg_gen_##OP##_i64);                        \

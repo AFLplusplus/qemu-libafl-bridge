2fb68e581604b1098f73956208d59e1666caff51
Romain Malmain <romain.malmain@pm.me>
2024-10-11 17:12:10 +0200

    do not create symlink if there is already something.

--- linker_interceptor.py
+++ linker_interceptor.py
@@ -40,7 +40,8 @@ def fix_compile_commands():
     with open("compile_commands.json", 'w') as f:
         f.write(res)
 
-    os.symlink("build/compile_commands.json", "../compile_commands.json")
+    if not os.path.isfile("../compile_commands.json"):
+        os.symlink("build/compile_commands.json", "../compile_commands.json")
 
 def process_args(args):
     global out_args, shareds, search, is_linking_qemu

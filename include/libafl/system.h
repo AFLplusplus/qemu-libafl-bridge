3fd96103735cf30011809008c2e9c6e752678d82
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2024-12-09 18:04:15 +0100

    Add systemmode hw_breakpoint libafl set/remove fns

--- include/libafl/system.h
+++ include/libafl/system.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include "hw/core/cpu.h"
+#include "gdbstub/enums.h"
+#include "sysemu/accel-ops.h"
+#include "sysemu/cpus.h"
+
+int libafl_qemu_set_hw_breakpoint(vaddr addr);
+int libafl_qemu_remove_hw_breakpoint(vaddr addr);
+
 void libafl_qemu_init(int argc, char** argv);
--- libafl/system.c
+++ libafl/system.c
@@ -3,4 +3,38 @@
 
 #include "libafl/system.h"
 
+int libafl_qemu_toggle_hw_breakpoint(vaddr addr, bool set);
+
 void libafl_qemu_init(int argc, char** argv) { qemu_init(argc, argv); }
+
+int libafl_qemu_set_hw_breakpoint(vaddr addr)
+{
+    return libafl_qemu_toggle_hw_breakpoint(addr, true);
+}
+
+int libafl_qemu_remove_hw_breakpoint(vaddr addr)
+{
+    return libafl_qemu_toggle_hw_breakpoint(addr, false);
+}
+
+int libafl_qemu_toggle_hw_breakpoint(vaddr addr, bool set) {
+    const int type = GDB_BREAKPOINT_HW;
+    const vaddr len = 1;
+    const AccelOpsClass *ops = cpus_get_accel();
+
+    CPUState* cs;
+    int ret = 0;
+
+    if (!ops->insert_breakpoint) {
+        return -ENOSYS;
+    }
+
+    CPU_FOREACH(cs) {
+        if (set) {
+            ret |= ops->insert_breakpoint(cs, type, addr, len);
+        } else {
+            ret |= ops->remove_breakpoint(cs, type, addr, len);
+        }
+    }
+    return ret;
+}
